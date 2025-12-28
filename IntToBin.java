b801e018ef9b54e9b00f6df9fb88e7a99e6dd3df
Dana Gabbay <dana.gabbay@post.runi.ac.il>
2025-12-28 19:57:20 +0200

    third

--- IntToBin.java
+++ IntToBin.java
@@ -10,7 +10,7 @@ public class IntToBin {
     public static String toBinary(int x) {
         String s= "0";
         if (x==0)
-            return s;
+            return "0";
         if (x%2==0)
             s = toBinary(x/2) + '0';
         else
