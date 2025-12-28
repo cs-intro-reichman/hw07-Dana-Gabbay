6b9c14ebc693926d13dd170a0634f0f959b73bec
Dana Gabbay <dana.gabbay@post.runi.ac.il>
2025-12-28 20:00:23 +0200

    forth

--- IntToBin.java
+++ IntToBin.java
@@ -11,6 +11,8 @@ public class IntToBin {
         String s= "0";
         if (x==0)
             return "0";
+        if (x==1)
+            return "1";
         if (x%2==0)
             s = toBinary(x/2) + '0';
         else
